90ee87ac4416b78dd61d5c1c1f491876a29c19ad
珍奶 <jary90705@gmail.com>
2021-11-12 11:21:40 +0800

    ItemTag relational method

--- app/Models/ItemTag.php
+++ app/Models/ItemTag.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ItemTag extends Model
 {
     protected $table = 'item_tags';
+
+    public function getItem() {
+        return $this->hasOne(Item::class, 'item_id', 'id')->get();
+    }
+
+    public function getTag() {
+        return $this->hasOne(Tag::class, 'tag_id', 'id')->get();
+    }
 }
