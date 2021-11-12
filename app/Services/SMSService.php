3e6a3186b57e524e00dde79e945a7313d979acc5
珍奶 <jary90705@gmail.com>
2021-11-12 22:49:42 +0800

    Controller & SMSService

---
+++ app/Services/SMSService.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Services;
+
+class SMSService {
+    public static function sendVerifyMessage($phone, $code) {
+        $msg = "牛脾趾書城會員驗證碼: ".$code."";
+        return SMSService::sendMessage($phone, $msg);
+    }
+    public static function sendMessage($phone, $msg) {
+        $sms_acc = env('SMS_ACCOUNT');
+        $sms_pwd = env('SMS_PASSWORD');
+        $sms_msg = urlencode($msg);
+        $return = file_get_contents('http://sms-get.com/api_send.php?method=1&sms_msg='.$sms_msg.'&phone='.$phone.'&username='.$sms_acc.'&password='.$sms_pwd.'');
+        return $return;
+    }
+    public static function getPoint() {
+        $sms_acc = env('SMS_ACCOUNT');
+        $sms_pwd = env('SMS_PASSWORD');
+        $return = file_get_contents('http://sms-get.com/api_query_credit.php?username='.$sms_acc.'&password='.$sms_pwd.'');
+        return $return;
+    }
+}
